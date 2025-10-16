a3e0fd1cdc46f91003aa16b71bd3191e6f2be85b
David Graham <david.graham@aize.io>
2025-10-16 16:53:26 +0100

    feat: add proxy to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      // Target is your backend API
+      "/api": {
+        target: "https://www.racefacer.com/ajax",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
