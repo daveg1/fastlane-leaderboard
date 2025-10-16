8114a6921b5f245356ba9d443dfa9bd3030fd5ea
David Graham <david.graham@aize.io>
2025-10-16 17:48:42 +0100

    feat: add error state

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -10,15 +10,20 @@ type Props = {
 export function List({ lapTimes, loading }: Readonly<Props>) {
   return (
     <div className="mx-auto flex max-w-xl flex-col items-center gap-2 text-white">
-      {loading &&
-        userIds.map(() => (
-          <div className="shimmer mx-auto h-10 w-full max-w-xl -skew-x-6 bg-slate-600 shadow-md"></div>
+      {(loading || !lapTimes) &&
+        userIds.map((id) => (
+          <div
+            key={id}
+            className="shimmer mx-auto h-10 w-full max-w-xl -skew-x-6 bg-slate-600 shadow-md"
+          ></div>
         ))}
 
       {!loading &&
         lapTimes.map((entry, index) => (
           <ListItem key={entry.name} {...entry} place={index} />
         ))}
+
+      {!loading && !lapTimes?.length && <div>Error fetching lap times</div>}
     </div>
   );
 }
