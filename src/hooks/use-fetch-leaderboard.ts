646f5adf2954899bc68ea8c08f8f20d47760b971
David Graham <david.graham@aize.io>
2025-10-16 18:22:38 +0100

    feat: add netlify toml

--- src/hooks/use-fetch-leaderboard.ts
+++ src/hooks/use-fetch-leaderboard.ts
@@ -4,9 +4,11 @@ import { useMemo } from "react";
 import type { ApiResponse, LapTime } from "../models/lap-time";
 import { userIds } from "../user-ids";
 
-const BASE_URL = import.meta.env.PROD
-  ? "https://www.racefacer.com/ajax"
-  : "/api";
+// const BASE_URL = import.meta.env.PROD
+//   ? "https://www.racefacer.com/ajax"
+//   : "/api";
+
+const BASE_URL = "/api";
 
 function extractData(data: ApiResponse) {
   const parser = new DOMParser();
