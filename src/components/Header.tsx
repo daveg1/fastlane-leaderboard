cc9548a5c2746b2932094921c6815f58e50d1363
David Graham <david.graham@aize.io>
2025-10-17 12:09:54 +0100

    feat: slide-in anim

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ type Props = {
 
 export function Header({ text }: Readonly<Props>) {
   return (
-    <header className="relative mx-auto mt-8 mb-4 flex max-w-fit items-center">
-      <span className="speed-line absolute top-1.5 -left-12 md:top-2"></span>
-      <span className="speed-line absolute top-3 -left-16 md:top-3.5"></span>
-      <span className="speed-line absolute bottom-2.5 -left-14"></span>
+    <header className="slide-in relative mx-auto mt-8 mb-4 flex max-w-fit items-center">
+      <span className="speed-line slide-in absolute top-1.5 -left-12 delay-1 md:top-2"></span>
+      <span className="speed-line slide-in absolute top-3 -left-16 delay-2 md:top-3.5"></span>
+      <span className="speed-line slide-in absolute bottom-2.5 -left-14 delay-3"></span>
 
       <h2 className="-skew-x-6 text-lg font-semibold text-white md:text-2xl">
         {text}
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -10,20 +10,22 @@ export function ListItem(data: Readonly<LapTime>) {
     >
       <span
         className={clsx(
-          "absolute -left-2 w-10 px-2 pr-4 font-semibold",
+          "absolute -left-2 w-10 px-2 pr-4 leading-5 font-semibold",
           data.place === 0 && "gold",
           data.place === 1 && "silver",
           data.place === 2 && "bronze",
         )}
       >
         {data.place + 1}
-        <span className="absolute top-0.5 text-xs">
+        <span className="absolute top-[1px] text-xs">
           {getOrdinal(data.place + 1)}
         </span>
       </span>
+
       <span className="ps-10 text-left text-sm font-semibold text-slate-200">
         {data.name}
       </span>
+
       <span className="font-semibold">{data.time}</span>
       <span className="text-xs text-slate-200">{data.date}</span>
     </div>
