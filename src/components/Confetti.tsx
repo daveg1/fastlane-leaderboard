3ccbb0b30d33d93880f55ea77022f46fbca0c70f
David Graham <david.graham@aize.io>
2026-08-05 11:04:51 +0100

    feat: increase confetti duration

--- src/components/Confetti.tsx
+++ src/components/Confetti.tsx
@@ -2,7 +2,7 @@ import ReactConfetti from "react-confetti";
 import { useCountdown } from "../hooks/use-delay";
 
 export function Confetti() {
-  const hasFinished = useCountdown(10_000);
+  const hasFinished = useCountdown(20_000);
 
   return <ReactConfetti recycle={!hasFinished} />;
 }
