d1c84fa773f1f800aea051ba89ff9bb080efbf4d
David G <davidgraham5642@gmail.com>
2026-08-16 23:29:44 +0100

    feat: anim for banner

--- src/components/SiteBanner.tsx
+++ src/components/SiteBanner.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export function SiteBanner({ children }: PropsWithChildren) {
   return (
-    <div className="bg-yellow-300 py-2 text-center font-semibold text-black">
+    <div className="flip-in flip-delay-10 bg-yellow-300 py-2 text-center font-semibold text-black">
       {children}
     </div>
   );
