e26a9dd20e6d206603437cc179ac8a02e8712c7e
David G <davidgraham5642@gmail.com>
2026-08-16 23:39:25 +0100

    feat: add link to racefacer dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,26 @@ function App() {
         </menu>
 
         <List lapTimes={lapTimes} loading={pending} />
+
+        <footer className="flex items-center justify-center gap-1 py-4 text-white opacity-90">
+          <a
+            className="border-b border-b-white/50 hover:border-b-white"
+            href="https://www.racefacer.com/dashboard"
+            target="_blank"
+          >
+            racefacer dashboard
+          </a>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="mb-1 size-4"
+          >
+            <path d="M6.22 8.72a.75.75 0 0 0 1.06 1.06l5.22-5.22v1.69a.75.75 0 0 0 1.5 0v-3.5a.75.75 0 0 0-.75-.75h-3.5a.75.75 0 0 0 0 1.5h1.69L6.22 8.72Z" />
+            <path d="M3.5 6.75c0-.69.56-1.25 1.25-1.25H7A.75.75 0 0 0 7 4H4.75A2.75 2.75 0 0 0 2 6.75v4.5A2.75 2.75 0 0 0 4.75 14h4.5A2.75 2.75 0 0 0 12 11.25V9a.75.75 0 0 0-1.5 0v2.25c0 .69-.56 1.25-1.25 1.25h-4.5c-.69 0-1.25-.56-1.25-1.25v-4.5Z" />
+          </svg>
+        </footer>
       </main>
     </>
   );
